501207796b22b8cb12382168832e16670e9577b3
vishalawasthi13 <vishal.awasthi@voltmoney.in>
2023-11-04 20:30:50 +0530

    removing error

--- src/main/java/practicetest.java
+++ src/main/java/practicetest.java
@@ -5,6 +5,7 @@ public class practicetest {
     public static void main(String args[]) {
         System.out.println("Hello");
         System.out.println("New Branch");
+        System.out.println("New BranchA");
 
     }
 }
